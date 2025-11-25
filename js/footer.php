0b007394485c12897b31ca56839e2e59aaf1c301
anidkabashi <anidkabashi@gmail.com>
2025-11-25 17:34:14 +0100

    .

---
+++ js/footer.php
@@ -0,0 +1,8 @@
+ <footer>
+        <nav>
+        <p>Digital school</p>
+        <?php wp_nav_menu( array( 'theme_location' => 'wp_devs_footer_menu', 'depth'=>  1));  ?>
+        </nav>
+    </footer>
+</body>
+</html>
