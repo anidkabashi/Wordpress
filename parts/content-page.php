e630e199865a2f6bf3d0cef11be83c16410772ee
anidkabashi <anidkabashi@gmail.com>
2025-11-27 17:40:28 +0100

    .

---
+++ parts/content-page.php
@@ -0,0 +1,9 @@
+<artical>
+    <header>
+        <h1><?php
+        //the_title();
+        ?></h1>
+    </header>
+    <?php the_content(); ?>
+    <?php wp_link_pages(); ?>
+</artical>
---
+++ parts/content-search.php
@@ -0,0 +1,7 @@
+<artical id="post-<?php the_ID(); ?>" <?php post_class();?>>
+
+<header>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+</header>
+
+</artical>
